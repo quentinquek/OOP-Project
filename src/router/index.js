b3ff66f484c377cf995d9b85992957beeee2fd8b
navakus <kavan.chua.2020@scis.smu.edu.sg>
2022-09-17 21:50:36 +0800

    Add: BookingDetails
    the details of single booking after click on a specific booking on My Bookings
    /my-bookings/booking-details

--- src/router/index.js
+++ src/router/index.js
@@ -4,87 +4,95 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 let routes = [
-	{
-		// will match everything
-		path: '*',
-		component: () => import('../views/404.vue'),
-	},
-	{
-		path: '/',
-		name: 'Home',
-		redirect: '/dashboard',
-	},
-	{
-		path: '/dashboard',
-		name: 'Dashboard',
-		layout: "dashboard",
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue'),
-	},
-	{
-		path: '/calendar',
-		name: 'Calendar',
-		layout: "dashboard",
-		component: () => import('../views/Calendar.vue'),
-	},
-	{
-		path: '/layout',
-		name: 'Layout',
-		layout: "dashboard",
-		component: () => import('../views/Layout.vue'),
-	},
-	{
-		path: '/my-bookings',
-		name: 'My Bookings',
-		layout: "dashboard",
-		component: () => import('../views/MyBookings.vue'),
-	},
-	{
-		path: '/manage-corporate-pass',
-		name: 'Manage Corporate Pass',
-		layout: "dashboard",
-		component: () => import('../views/ManageCorporatePass.vue'),
-	},
-	{ // not in use
-		path: '/billing',
-		name: 'Billing',
-		layout: "dashboard",
-		component: () => import('../views/Billing.vue'),
-	},
-	{
-		path: '/rtl',
-		name: 'RTL',
-		layout: "dashboard-rtl",
-		meta: {
-			layoutClass: 'dashboard-rtl',
-		},
-		component: () => import('../views/RTL.vue'),
-	},
-	{
-		path: '/Profile',
-		name: 'Profile',
-		layout: "dashboard",
-		meta: {
-			layoutClass: 'layout-profile',
-		},
-		component: () => import('../views/Profile.vue'),
-	},
-	{
-		path: '/sign-in',
-		name: 'Sign-In',
-		component: () => import('../views/Sign-In.vue'),
-	},
-	{
-		path: '/sign-up',
-		name: 'Sign-Up',
-		meta: {
-			layoutClass: 'layout-sign-up',
-		},
-		component: () => import('../views/Sign-Up.vue'),
-	},
-]
+  {
+    // will match everything
+    path: "*",
+    component: () => import("../views/404.vue"),
+  },
+  {
+    path: "/",
+    name: "Home",
+    redirect: "/dashboard",
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    layout: "dashboard",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ "../views/Dashboard.vue"),
+  },
+  {
+    path: "/calendar",
+    name: "Calendar",
+    layout: "dashboard",
+    component: () => import("../views/Calendar.vue"),
+  },
+  {
+    path: "/layout",
+    name: "Layout",
+    layout: "dashboard",
+    component: () => import("../views/Layout.vue"),
+  },
+  {
+    path: "/my-bookings",
+    name: "My Bookings",
+    layout: "dashboard",
+    component: () => import("../views/MyBookings.vue"),
+  },
+  {
+    path: "/my-bookings/booking-details",
+    name: "Booking details",
+    layout: "dashboard",
+    component: () => import("../views/BookingDetails.vue"),
+  },
+  {
+    path: "/manage-corporate-pass",
+    name: "Manage Corporate Pass",
+    layout: "dashboard",
+    component: () => import("../views/ManageCorporatePass.vue"),
+  },
+  {
+    // not in use
+    path: "/billing",
+    name: "Billing",
+    layout: "dashboard",
+    component: () => import("../views/Billing.vue"),
+  },
+  {
+    path: "/rtl",
+    name: "RTL",
+    layout: "dashboard-rtl",
+    meta: {
+      layoutClass: "dashboard-rtl",
+    },
+    component: () => import("../views/RTL.vue"),
+  },
+  {
+    path: "/Profile",
+    name: "Profile",
+    layout: "dashboard",
+    meta: {
+      layoutClass: "layout-profile",
+    },
+    component: () => import("../views/Profile.vue"),
+  },
+  {
+    path: "/sign-in",
+    name: "Sign-In",
+    component: () => import("../views/Sign-In.vue"),
+  },
+  {
+    path: "/sign-up",
+    name: "Sign-Up",
+    meta: {
+      layoutClass: "layout-sign-up",
+    },
+    component: () => import("../views/Sign-Up.vue"),
+  },
+];
 
 // Adding layout property from each route to the meta
 // object so it can be accessed later.
---
+++ src/views/BookingDetails.vue
@@ -0,0 +1,36 @@
+<!-- 
+	This is the tables page, it uses the dashboard layout in: 
+	"./layouts/Dashboard.vue" .
+ -->
+
+<template>
+  <div>
+    <!-- Authors Table -->
+    <a-row :gutter="24" type="flex">
+      <!-- Authors Table Column -->
+      <a-col :span="24" class="mb-24">
+        <!-- Authors Table Card -->
+        <CardAuthorTable
+          :data="table1Data"
+          :columns="table1Columns"
+        ></CardAuthorTable>
+        <!-- / Authors Table Card -->
+      </a-col>
+      <!-- / Authors Table Column -->
+    </a-row>
+  </div>
+</template>
+
+<script>
+// "Authors" table component.
+import CardAuthorTable from "../components/Cards/CardBookingDetails";
+
+
+export default {
+  components: {
+    CardAuthorTable,
+  },
+};
+</script>
+
+<style lang="scss"></style>
